9ed8abeed644d08843fd5f61d0f7a6963b0f156d
モハメド <12945919+iamrare@users.noreply.github.com>
2019-09-04 19:05:31 +0900

    migrate to AndroidX and delete sample app

--- binder/src/main/java/jp/satorufujiwara/binder/recycler/BaseRecyclerBinderAdapter.java
+++ binder/src/main/java/jp/satorufujiwara/binder/recycler/BaseRecyclerBinderAdapter.java
@@ -1,6 +1,6 @@
 package jp.satorufujiwara.binder.recycler;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
